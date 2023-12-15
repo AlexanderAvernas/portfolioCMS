43c8cd7b25231f7f8dab54f08369517f0dd5167e
Alexander-Avernas <AlexanderAvernas@users.noreply.github.com>
2023-12-15 14:33:58 +0100

    started project and styling

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,16 +1,62 @@
 import * as React from "react"
 import { Link } from "gatsby"
+import { graphql, useStaticQuery } from "gatsby"
+import { GatsbyImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 
 //sidans namn blir portfolio efter namnet på javascript-filen
-const SecondPage = () => (
+const SecondPage = () => {
+
+    const data = useStaticQuery(graphql`
+    query {
+        allContentfulProfileItem {
+            edges {
+              node{
+                title
+                techniques
+                description {
+                  description
+                }
+                itemImage {
+                  gatsbyImage(width:250)
+                }
+              }
+            }
+          }
+    }
+    `
+    )
+
+    return (
   <Layout>
-    <h1>Hi from the second page</h1>
+    <div className="portfolioContainer">
+    <h1>Portfolio</h1>
+    {data.allContentfulProfileItem.edges.map((edge) => {
+        return (
+            <div className="portfolioInnerContainer" key={edge.node.id} >
+                <div className="portfolioTextContainer">
+                <h2>{edge.node.title}</h2>
+                <p>{edge.node.description.description}</p>
+                <ul className="portfolioListContainer">
+                    {edge.node.techniques.map((technique, index) => (
+                        <li className="portfolioListItem" key={index}>{technique}</li>
+                    ))}
+                    </ul>
+                    </div>
+                <GatsbyImage alt="edge.node.title" image={edge.node.itemImage.gatsbyImage} ></GatsbyImage>
+
+
+            </div>
+
+        )
+    })}
+    </div>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+    </Layout>
+    )
+}
 
 export const Head = () => <title>Portfolio Page</title>
 
