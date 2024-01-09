0ae73d4da7852b0180a90625eb7ebfd28e219ec6
Alexander-Avernas <AlexanderAvernas@users.noreply.github.com>
2024-01-09 13:46:07 +0100

    made 404 dynamic

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,22 +1,34 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import Layout from '../components/layout';
+import { useStaticQuery, graphql } from "gatsby"
 
-const NotFoundPage = () => (
+const NotFoundPage = () => {
+    const data = useStaticQuery(graphql`
+    query {
+      contentfulPagenotfound {
+        title
+        description
+        notfoundlink
+      }
+    }
+  `)
+
+    return (
   <Layout>
     <div className="noPageFoundOutContainer">
     <div className="noPageFoundContainer">
-      <h1 className="noPageFoundTitle">Page not found</h1>
+      <h1 className="noPageFoundTitle">{data.contentfulPagenotfound.title}</h1>
       <p className="noPageFoundMessage">
-        The page you are looking for might have been removed, had its name changed,
-        or is temporarily unavailable.
+      {data.contentfulPagenotfound.description}
       </p>
-      <Link to="/" className="noPageFoundLink">
+      <Link to={data.contentfulPagenotfound.notfoundlink} className="noPageFoundLink">
         Go to Home Page
       </Link>
     </div>
     </div>
   </Layout>
-);
+  )
+};
 
 export default NotFoundPage;
