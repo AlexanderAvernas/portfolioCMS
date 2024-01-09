d0c025370fce96ff3ddab9f507a5b9ff7a2acbaf
Alexander-Avernas <AlexanderAvernas@users.noreply.github.com>
2024-01-09 12:34:23 +0100

    test to netlify

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -15,7 +15,7 @@ export const query = graphql`
         description
       }
       itemImage {
-        gatsbyImage(width: 250)
+        gatsbyImage(layout: CONSTRAINED, width: 250)
       }
     }
   }
